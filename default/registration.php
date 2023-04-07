94f834f35cacf3945cc8757dcf38efafa19c7f64
hoanghai1506 <luonghoanghai2002@gmail.com>
2023-04-07 13:24:46 +0700

    challenge 1

---
+++ default/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2023 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Tigren/default',
+    __DIR__
+);
